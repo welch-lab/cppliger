da2e5b9061e54823be1add1793112d5252651ecd
Andrew Robbins <andrew@robbinsa.me>
2022-11-11 10:00:45 -0500

    Add project files.

---
+++ cppliger/cppliger.cpp
@@ -0,0 +1,12 @@
+﻿// cppliger.cpp : Defines the entry point for the application.
+//
+
+#include "cppliger.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
